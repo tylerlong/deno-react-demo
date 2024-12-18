6b485a85d85c9a2e767b15c907d6bbc354d36aea
Tyler Liu <tyler4long@gmail.com>
2024-12-17 23:03:07 -0800

    Update react version

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import React, { StrictMode } from "https://esm.sh/react@18.2.0";
-import { createRoot } from "https://esm.sh/react-dom@18.2.0/client";
+import React, { StrictMode } from "https://esm.sh/react@18.3.1";
+import { createRoot } from "https://esm.sh/react-dom@18.3.1/client";
 
 const root = createRoot(document.getElementById("root")!);
 root.render(
